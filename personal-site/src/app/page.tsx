5e15e026cb68f14e44dbd4e12ac11aeedc478eb9
cscheppele <cmscheppele@gmail.com>
2023-07-11 19:44:57 +0200

    components set up with navigation

--- personal-site/src/app/page.tsx
+++ personal-site/src/app/page.tsx
@@ -1,18 +1,29 @@
-import styles from './page.module.css'
-import { PageComponent } from '@/types'
-import { useState } from 'react'
+"use client"
+import styles from './page.module.css';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import Hobbies from '@/components/Hobbies';
+import PersonalProjects from '@/components/PersonalProjects';
+import Politics from '@/components/Politics';
+import ProfessionalHistory from '@/components/ProfessionalHistory';
+import { PageComponent } from '@/types';
+import { useState } from 'react';
+import { MouseEvent } from 'react';
 
 export default function Home() {
   const [selectedComponent, setSelectedComponent] = useState<PageComponent>("Hero")
   
-
-
-  const visibleComponent = (selectedComponent === "Hero" ? <Hero selected={setSelectedComponent}/> 
-    : selectedComponent === "About" ? <About selected={setSelectedComponent}/> 
-    : selectedComponent === "Hobbies" ? <Hobbies selected={setSelectedComponent}/>
-    : selectedComponent === "PersonalProjects" ? <PersonalProjects selected={setSelectedComponent}/>
-    : selectedComponent === "Politics" ? <Politics selected={setSelectedComponent}/>
-    : selectedComponent === "ProfessionalHistory" ? <ProfessionalHistory selected={setSelectedComponent}/>
+  // function handleClick (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>): void {
+  //   console.log((event.target as any).value, (event.target as any).name)
+  //   setSelectedComponent((event.target as any).name)
+  // }
+  
+  const visibleComponent = (selectedComponent === "Hero" ? <Hero setSelectedComponent={setSelectedComponent}/> 
+    : selectedComponent === "About" ? <About setSelectedComponent={setSelectedComponent}/> 
+    : selectedComponent === "Hobbies" ? <Hobbies setSelectedComponent={setSelectedComponent}/>
+    : selectedComponent === "PersonalProjects" ? <PersonalProjects setSelectedComponent={setSelectedComponent}/>
+    : selectedComponent === "Politics" ? <Politics setSelectedComponent={setSelectedComponent}/>
+    : selectedComponent === "ProfessionalHistory" ? <ProfessionalHistory setSelectedComponent={setSelectedComponent}/>
     : "Cannot find Component")
   
   return (
--- personal-site/src/components/Hobbies.tsx
+++ personal-site/src/components/Hobbies.tsx
@@ -1,17 +1,12 @@
-import Image from 'next/image'
-import face from "../../public/cropped-face.png"
-import styles from "@/styles/Hero.module.css"
+import styles from "@/styles/Hobbies.module.css"
 import { Dispatch, SetStateAction } from 'react'
 import { PageComponent } from '@/types'
 
-export default function Hero ({setSelectedComponent}:{setSelectedComponent: Dispatch<SetStateAction<PageComponent>>}) {
+export default function Hobbies ({setSelectedComponent}:{setSelectedComponent: Dispatch<SetStateAction<PageComponent>>}) {
     return (
       <main className={styles.main}>
-        <div className={styles.face}>
-          <Image src={face} layout='fill' objectFit='cover' alt="profile picture"/>
-        </div>
         <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("PersonalProjects")}>Personal Projects</button>
-        <button className={`${styles.button} ${styles.button_top}`} onClick={() => setSelectedComponent("Hobbies")}>Hobbies</button>
+        <button className={`${styles.button} ${styles.button_top}`} onClick={() => setSelectedComponent("Hero")}>Home</button>
         <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("ProfessionalHistory")}>Professional History</button>
         <button className={`${styles.button} ${styles.button_top}`} onClick={() => setSelectedComponent("About")}>About</button>
         <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("Politics")}>Political Stances</button>
