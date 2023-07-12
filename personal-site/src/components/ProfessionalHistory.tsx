9b7c40e2f24f037227c51b22889c0f297b35079b
cscheppele <cmscheppele@gmail.com>
2023-07-12 13:26:16 +0200

    Hero reworked

--- personal-site/src/components/ProfessionalHistory.tsx
+++ personal-site/src/components/ProfessionalHistory.tsx
@@ -4,12 +4,25 @@ import { PageComponent } from '@/types'
 
 export default function ProfessionalHistory ({setSelectedComponent}:{setSelectedComponent: Dispatch<SetStateAction<PageComponent>>}) {
     return (
-      <main className={styles.main}>
-        <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("PersonalProjects")}>Personal Projects</button>
-        <button className={`${styles.button} ${styles.button_top}`} onClick={() => setSelectedComponent("Hobbies")}>Hobbies</button>
-        <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("Hero")}>Home</button>
-        <button className={`${styles.button} ${styles.button_top}`} onClick={() => setSelectedComponent("About")}>About</button>
-        <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("Politics")}>Political Stances</button>
-      </main>
+      <div className={styles.main}>
+        <div className={styles.professional_section}>EDUCATION</div>
+        <div className={styles.professional_section}>WORK HISTORY</div>
+        <div className={styles.professional_section}>KNOWN TECH</div>
+        <div className={styles.professional_section}>SPOKEN LANGUAGES</div>
+        <div className={styles.professional_section}>CODING LANGUAGES</div>
+        <div className={styles.professional_section}>SKILLS</div>
+        Want to hire me and need somthing for your files? Check out my CV
+
+        <div className={styles.navbar}>
+        <h1 className={styles.navigate}>Navigate</h1>
+            <div className={styles.button_container}>
+                <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("PersonalProjects")}>Personal Projects</button>
+                <button className={`${styles.button} ${styles.button_top}`} onClick={() => setSelectedComponent("Hero")}>Home</button>
+                <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("ProfessionalHistory")}>Professional History</button>
+                <button className={`${styles.button} ${styles.button_top}`} onClick={() => setSelectedComponent("About")}>About</button>
+                <button className={`${styles.button} ${styles.button_bottom}`} onClick={() => setSelectedComponent("Politics")}>Political Stances</button>
+            </div>
+        </div>
+      </div>
     )
 }
