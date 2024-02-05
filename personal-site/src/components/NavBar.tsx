17a1d8e00bc966d12594f1199ec1d0fb5a4f36c9
CScheppele <cmscheppele@gmail.com>
2024-02-04 21:22:44 -0600

    reorganisation, or components.

--- personal-site/src/components/NavBar.tsx
+++ personal-site/src/components/NavBar.tsx
@@ -6,6 +6,10 @@ export default function NavBar () {
             <button className={styles.button}>Home</button>
             <button className={styles.button}>Portfolio</button>
             <button className={styles.button}>Hobbies</button>
+            <div className={styles.descriptor_box}>
+                <h1 className={`${styles.name} ${styles.text}`}>Chris Scheppele</h1>
+                <div className={styles.underline}></div>
+            </div>
             <button className={styles.button}>Curriculum Vitae</button>
             <button className={styles.button}>About Me</button>
             <button className={styles.button}>Political Stances</button>
